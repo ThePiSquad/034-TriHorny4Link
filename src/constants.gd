20b2b5ea210697b57b1ecea0925a2b5f68aeed1b
Dragonzhi <3248569738@qq.com>
2026-03-08 22:51:45 +0800

    refactor(常量): 重构颜色相关常量并添加颜色映射功能
    将Enermy类重命名为EnemyConstants，增加COLOR_MAP字典和get_color方法
    为conduit.tscn中的ShapeDrawer节点添加填充颜色

--- src/constants.gd
+++ src/constants.gd
@@ -11,7 +11,22 @@ static var generator_reserved_coords:Array[GridCoord]=[
 
 const default_crystal_energy_level: int = 2
 
-
-class Enermy:
+class EnemyConstants:
 	const default_size: float = 1.0
 	const default_color: Color = Color.CYAN
+
+static var COLOR_MAP = {
+	Enums.ColorType.WHITE: Color.WHITE,
+	Enums.ColorType.BLACK: Color.BLACK,
+	Enums.ColorType.RED: Color("ff4545ff"),
+	Enums.ColorType.BLUE: Color("4587ffff"),
+	Enums.ColorType.YELLOW: Color("ffde45ff"),
+	Enums.ColorType.GREEN: Color.GREEN,
+	Enums.ColorType.ORANGE: Color.ORANGE,
+	Enums.ColorType.PURPLE: Color.PURPLE,
+}
+
+static func get_color(type: Enums.ColorType) -> Color:
+	if COLOR_MAP.has(type):
+		return COLOR_MAP[type]
+	return Color.MAGENTA # 如果找不到，返回一个显眼的紫色提醒出错了
